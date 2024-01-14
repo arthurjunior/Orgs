30e6dbc96b038f6ff39578e3023220d338936232
Arthur Junior <dev.arthurcarvalho@gmail.com>
2024-01-14 15:41:22 -0400

    Melhorando codigo com o aplly para remover a palavra binding de varias linha

--- app/src/main/java/br/com/alura/orgs/ui/dialog/FormularioImagemDialog.kt
+++ app/src/main/java/br/com/alura/orgs/ui/dialog/FormularioImagemDialog.kt
@@ -3,36 +3,38 @@ package br.com.alura.orgs.ui.dialog
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
-import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
 import br.com.alura.orgs.databinding.FormularioImagemBinding
 import br.com.alura.orgs.extensions.tentaCarregarImagem
 
 class FormularioImagemDialog (private val context: Context) {
-    fun dialog(urlPadrao: String? = null, quandoImagemCarregada: (imagem: String) -> Unit) {
-        val binding = FormularioImagemBinding
-            .inflate(LayoutInflater.from(context))
-        urlPadrao?.let {
-            binding.formularioImagemImagemview.tentaCarregarImagem(it)
-            binding.formularioImagemUrl.setText(it)
-        }
-        binding.formularioImagemBotaoCarregar.setOnClickListener {
-            val url = binding.formularioImagemUrl.text.toString()
-            binding.formularioImagemImagemview.tentaCarregarImagem(url)
-        }
+    fun dialog(
+        urlPadrao: String? = null,
+        quandoImagemCarregada: (imagem: String) -> Unit
+    ) {
+        FormularioImagemBinding
+            .inflate(LayoutInflater.from(context)).apply {
+                urlPadrao?.let {
+                    formularioImagemImagemview.tentaCarregarImagem(it)
+                    formularioImagemUrl.setText(it)
+                }
+                formularioImagemBotaoCarregar.setOnClickListener {
+                    val url = formularioImagemUrl.text.toString()
+                    formularioImagemImagemview.tentaCarregarImagem(url)
+                }
 
-        AlertDialog.Builder(context)
-            .setView(binding.root)
-            .setPositiveButton("Confirmar") { _, _ ->
-                val url = binding.formularioImagemUrl.text.toString()
-                Log.i("FormularioImagemDialog", "dialog: $url")
-                quandoImagemCarregada(url)
+                AlertDialog.Builder(context)
+                    .setView(root)
+                    .setPositiveButton("Confirmar") { _, _ ->
+                        val url = formularioImagemUrl.text.toString()
+                        Log.i("FormularioImagemDialog", "dialog: $url")
+                        quandoImagemCarregada(url)
+                    }
+                    .setNegativeButton("Cancelar") { _, _ ->
+
+                    }
+                    .show()
             }
-            .setNegativeButton("Cancelar") { _, _ ->
-
-            }
-            .show()
-
     }
 }
 
