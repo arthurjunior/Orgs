e14b980c44b5d9e1c3497032710c1900d44cf1be
Arthur Junior <dev.arthurcarvalho@gmail.com>
2024-01-25 13:30:28 -0400

    Modulando cores da splashScreen

--- app/src/main/java/br/com/alura/orgs/ui/activity/SplashScreenActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/SplashScreenActivity.kt
@@ -1,12 +1,24 @@
 package br.com.alura.orgs.ui.activity
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import br.com.alura.orgs.R
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+supportActionBar?.hide()
+        window.statusBarColor = Color.parseColor("#94a84e")
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,ListaProdutosActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
 }
